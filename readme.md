ef306facd42b885c4ceb05061de05fb2f87d9697
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-08-06 16:38:44 +0530

    Todo App

--- readme.md
+++ readme.md
@@ -3,9 +3,14 @@
 
 This Income Expense Calculator is built using HTML, CSS and Javascript.
 
-1) In this Calculator there are three input field to get input from user.
-2) They are Income, Description and Expense.
-3) Once all the values are entered and submit is clicked then net balance is calculated based on the input values.
-4) And this will be continued for n number of inputs.
+1) In this Calculator there are three input fields to get income and expense details.
+2) They are Type, Description and Amount.
+3) Once all the values are entered and submit is clicked then the entries will be displayed on screen.
+4) And with this n number of inputs can created.
+5) There is also three radio buttons namely all, income and expense.
+6) Once all is clicked then all the entered details will be displayed.
+7) And when income is clicked all details related to income type is displayed.
+8) And when expense is clicked all details related to expense type is displayed.
+9) Here a user can Create and Read the data.
 
 
--- script.js
+++ script.js
@@ -1,65 +1,160 @@
-let income = document.getElementById("income");
+let type = document.getElementById("type");
 let desc = document.getElementById("desc");
-let expense = document.getElementById("expense");
+let amount = document.getElementById("amount");
+
+
+
+let all_radio = document.getElementById("all_radio");
+let income_radio = document.getElementById("income_radio");
+let expense_radio = document.getElementById("expense_radio");
+let datadisplay = document.getElementById("datadisplay");
+
+
+
+let editbtn = document.getElementById("editbtn");
+let deletebtn = document.getElementById("deletebtn");
+
+
 
 
 let maincontainer = document.getElementById("main");
+let main2container = document.getElementById("main2");
+let listelement = document.createElement("div");
+let listcontainer = document.getElementById("listcont");
+
 
 
 let submit = document.getElementById("submit");
 let card = document.createElement("div");
 let card1 = document.createElement("div");
+let card2 = document.createElement("div");
 
-let bal=[];
+
+
+let i=0;
+    let incomeres=0;
+    let expenseres=0;
+    let netbalance=0;
 let arr=[];
+let arr2=[];
 submit.addEventListener("click",()=>{
+    
     const alldetails = {
-        income_value :  income.value,
+        type_value :  type.value,
         desc_value : desc.value,
-        expense_value : expense.value,
+        amount_value : amount.value,
     };
 
     arr.push(alldetails);
-    console.log(arr);
 
     card.innerHTML +=`
-    <div class="flex flex-row gap-15">
-    <div>${alldetails.income_value}</div>
+    <span class="flex flex-row gap-15 pt-10">
+    <div>${alldetails.type_value}</div>
     <div>${alldetails.desc_value}</div>
-    <div>${alldetails.expense_value}</div>
-    <div>${alldetails.income_value-alldetails.expense_value}</div>
-    </div>
+    <div>${alldetails.amount_value}</div>
+    </span>
     `;
+    // console.log(arr);
+    // console.log(typeof(alldetails.amount_value));
+
 
     
-
-    let res=alldetails.income_value - alldetails.expense_value;
-    
-    bal.push(res);
-    
-    let i,j=0;
-    for(i=0;i<bal.length;i++)
+    while(i<arr.length)
     {
-        j = j + (bal[i]);
+    if((alldetails.type_value) == "income"){
+        incomeres = incomeres + Number((alldetails.amount_value));
+        //console.log(typeof(incomeres));
+       
+    }
+    else if((alldetails.type_value) == "expense")
+    {
+        expenseres = expenseres + Number((alldetails.amount_value));
+        
+    }
+    else{
+        console.log("enter the value");
+    }
+    netbalance = incomeres-expenseres;
+    i++;
+    
     }
 
     card1.innerHTML +=`
-    <div class="font-bold">Net Balance : </div>
-    <div>${j}</div>
-    `;  
+    <div class="flex flex-row gap-10">
+    <div>Total Income : </div>
+    <div>${incomeres-expenseres}</div>
+    <div>Total Expense : </div>
+    <div>${expenseres}</div>
+    <div>Net Balance : </div>
+    <div>${netbalance}</div>
+    </div>
+    `;
+
+    let arr2=[];
+    all_radio.addEventListener("click",()=>{
+   
+    let alldetails2 = alldetails;
+    console.log(alldetails2);
+    arr2=arr;
+    console.log(arr2);
+    card2.innerHTML +=`
+    <div class = "font-semibold pl-120 flex flex-row pt-10 gap-15">
+    <div>All Details</div>
+    <div>${alldetails2.type_value}</div>
+    <div>${alldetails2.desc_value}</div>
+    <div>${alldetails2.amount_value}</div>
+    </div>
+    `;
+});
+
+
+income_radio.addEventListener("click",()=>{
+    
+    let alldetails2 = alldetails;
+    console.log(alldetails2);
+    arr2=arr;
+    console.log(arr2);
+    if(alldetails2.type_value=="income")
+    {
+    card2.innerHTML +=`
+    <div class = "font-semibold pl-120 flex flex-row pt-10 gap-15">
+    <div>Income Details</div>
+    <div>${alldetails2.type_value}</div>
+    <div>${alldetails2.desc_value}</div>
+    <div>${alldetails2.amount_value}</div>
+    </div>
+    `;
+    }
+});
+
+
+
+expense_radio.addEventListener("click",()=>{
+   
+    let alldetails2 = alldetails;
+    console.log(alldetails2);
+    arr2=arr;
+    console.log(arr2);
+    if(alldetails2.type_value=="expense")
+    {
+    card2.innerHTML +=`
+    <div class = "font-semibold pl-120 flex flex-row pt-10 gap-15">
+    <div>Expense Details</div>
+    <div>${alldetails2.type_value}</div>
+    <div>${alldetails2.desc_value}</div>
+    <div>${alldetails2.amount_value}</div>
+    </div>
+    `;
+    }
+});
 
 
 });
-
 
 submit.addEventListener("click",()=>{
-    income.value="";
+    type.value="";
     desc.value="";
-    expense.value="";
+    amount.value="";
 });
 
-
-
-
-
-maincontainer.append(card,card1);
+maincontainer.append(card,card1,card2);
